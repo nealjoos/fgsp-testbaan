500822f97ba32089f96176b38413376beddd7b94
nealjoos <47505324+nealjoos@user.noreply.gihub.com>
2019-04-26 21:31:50 +0200

    :pencil2: richting

--- javascript.js
+++ javascript.js
@@ -6,3 +6,23 @@ function getCookie(name) {
   var value = re.exec(document.cookie);
   return (value != null) ? unescape(value[1]) : null;
 }
+
+$(document).ready(function() {
+  let richting = getCookie("richting");
+  if (richting === 'CCW') {
+    document.getElementById('AS1').style.transform = 'rotate(180deg)';
+    document.getElementById('AS2').style.transform = "rotate(180deg)";
+    document.getElementById('AS3').style.transform = "rotate(0deg)";
+    document.getElementById('AS4').style.transform = "rotate(0deg)";
+    document.getElementById('AS5').style.transform = 'rotate(-90deg)';
+    document.getElementById('AS6').style.transform = "rotate(90deg)";
+  } else if (richting === 'CW') {
+    document.cookie = "richting=CW";
+    document.getElementById('AS1').style.transform = 'rotate(0deg)';
+    document.getElementById('AS2').style.transform = "rotate(0deg)";
+    document.getElementById('AS3').style.transform = "rotate(180deg)";
+    document.getElementById('AS4').style.transform = "rotate(180deg)";
+    document.getElementById('AS5').style.transform = 'rotate(90deg)';
+    document.getElementById('AS6').style.transform = "rotate(-90deg)";
+  }
+});
